85613f92bf42456b1de79750a748b33e1ae60700
Bryan Ferris <saffsnail@gmail.com>
2018-02-13 12:19:33 -0800

    Added whitespace tests to exactness module

--- src/parse/num/exactness.rs
+++ src/parse/num/exactness.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub enum Exactness {
     Exact,
     Inexact
@@ -11,4 +12,29 @@ named! { pub exact<Exactness>,
 named! { pub inexact<Exactness>,
          map!(tag!("#i"),
               |_| Exactness::Inexact)
+}
+
+named! { pub exactness<Exactness>,
+         ws!(alt!(exact | inexact))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use nom::IResult;
+
+    #[test]
+    fn leading_whitespace() {
+        assert_eq!(exactness(&b" \t\t \n #i"[..]), IResult::Done(&b""[..], Exactness::Inexact));
+    }
+
+    #[test]
+    fn trailing_whitespace() {
+        assert_eq!(exactness(&b"#i \t  \n\n  "[..]), IResult::Done(&b""[..], Exactness::Inexact));
+    }
+
+    #[test]
+    fn surrounding_whitespace() {
+        assert_eq!(exactness(&b"   \t\t\t\n\n\n#i\n\n\n\t\t\t    "[..]), IResult::Done(&b""[..], Exactness::Inexact));
+    }
 }
