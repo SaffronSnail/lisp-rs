695757dbaa3d3be648660fdf16fd93d7cdbf2e6d
Bryan Ferris <saffsnail@gmail.com>
2018-04-24 00:14:15 -0500

    Added proper namespace to repl construction

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate lisp;
 
 pub fn main() {
-    lisp::Repl::new().run();
+    lisp::repl::Repl::new().run();
 }
--- src/parse/num/mod.rs
+++ src/parse/num/mod.rs
@@ -2,3 +2,5 @@ pub mod exactness;
 pub mod radix;
 pub mod sign;
 
+pub use self::exactness::Exactness;
+pub use self::sign::Sign;
