c7c9b3d5a2c8afa8d4d6b4ef36e988ddd843bda4
Bryan Ferris <saffsnail@gmail.com>
2018-04-24 20:07:30 -0500

    Added partial implementation of number parser

--- src/parse/num/mod.rs
+++ src/parse/num/mod.rs
@@ -4,3 +4,20 @@ pub mod sign;
 
 pub use self::exactness::Exactness;
 pub use self::sign::Sign;
+
+pub struct Number {
+    sign: Sign,
+    exactness: Exactness,
+}
+
+use self::exactness::exactness;
+use self::sign::sign;
+
+named!{pub num<Number>,
+       map!(tuple!(sign, exactness),
+            |result| Number {
+                sign: result.0,
+                exactness: result.1
+            }
+       )
+}
